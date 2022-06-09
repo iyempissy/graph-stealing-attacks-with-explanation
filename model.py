83ed630a78c1f88251967e4cb230bf3dc289e337
iyempissy <9529101+iyempissy@users.noreply.github.com>
2022-06-09 16:00:25 +0200

    License removed for repository anonymization

--- model.py
+++ model.py
@@ -1,9 +1,4 @@
-# Copyright (c) 2020-present, Royal Bank of Canada.
-# All rights reserved.
-#
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-#
+# License removed for repository anonymization
 import math
 
 from graph_generator import *
