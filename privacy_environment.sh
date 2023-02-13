2a6b4225f25e1f2c797b46837f70ac5893144dfa
iyempissy <9529101+iyempissy@users.noreply.github.com>
2023-02-13 20:19:33 +0100

    Create privacy_environment.sh

---
+++ privacy_environment.sh
@@ -0,0 +1,14 @@
+set -e
+
+
+conda env create --file privacy_environment.yml --force
+
+
+conda activate privacy
+TORCH=1.9.1
+CUDA=cu111
+python -m pip install torch-scatter --no-cache-dir --no-index --find-links https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
+python -m pip install torch-sparse --no-cache-dir --no-index --find-links https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
+python -m pip install torch-cluster --no-cache-dir --no-index --find-links https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
+python -m pip install torch-spline-conv --no-cache-dir --no-index --find-links https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
+python -m pip install torch-geometric --no-cache-dir
